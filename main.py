861460736e9958cb82d88a00f7d75025bf4fabc7
Sebastian Zasuwa <sebastianzasuwa@gmail.com>
2020-12-02 18:07:26 +0100

    Refactoring and preparing for CPE merge

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ def fetch_nvd_page(query):
     output = []
     idx_max = 1
     idx_current = 0
+    safe_query = urllib.parse.quote(query)
+
     while idx_current < idx_max:
-        url = 'https://nvd.nist.gov/vuln/search/results?query=%s&results_type=overview&form_type=Basic&search_type=all&startIndex=%i' % (query, idx_current)
-        print(idx_current, idx_max, url)
+        url = 'https://nvd.nist.gov/vuln/search/results?query=%s&startIndex=%i' % (safe_query, idx_current)
+        print(url)
         site = requests.get(url)
-        idx_max = int(re.search(r'(?<=data-testid="vuln-matching-records-count">)\d+[,]?\d*', site.text).group().replace(",",""))
-        idx_current = int((re.search(r'(?<=data-testid="vuln-displaying-count-through">)\d+[,]?\d*', site.text).group()).replace(",",""))
+        idx_max = int(re.search(r'(?<=data-testid="vuln-matching-records-count">)\d+[,]?\d*', site.text).group().replace(",", ""))
+        idx_current = int((re.search(r'(?<=data-testid="vuln-displaying-count-through">)\d+[,]?\d*', site.text).group()).replace(",", ""))
         output += parse_nvd_page(site.text)
     return output
-
 
 
 def parse_nvd_page(page):
@@ -42,7 +43,8 @@ def parse_nvd_page(page):
         output.append(row)
     return output
 
-def createCSV(filename, tab):
+
+def export_to_csv(filename, tab):
     row = ["CVE ID", "Description", "Published Date", "Severity 3.0", "Severity 2.0"]
     tab.insert(0, row)
     with xlsxwriter.Workbook(filename + '.xlsx') as workbook:
@@ -50,80 +52,9 @@ def createCSV(filename, tab):
         for row_num, data in enumerate(tab):
             worksheet.write_row(row_num, 0, data)
 
-tmp = fetch_nvd_page("wordpress")
+
+tmp = fetch_nvd_page("cpe:2.3:*:*:jquery:2.1.4")
 print(len(tmp))
 for x in tmp:
     print(x)
-createCSV("testFile", tmp)
-
-def getSitePossibleLibList(url):
-    page = requests.get(url)
-    scripts = re.findall(r'<script[^>]+>\s*</script>', page.text)
-
-    output = []
-    for script in scripts:
-        lib = re.search(r'(?<=[/"])[^/]+(?=\.js)', script)
-
-        if lib is None:
-            continue
-
-        lib = lib.group()
-        ver = re.search(r'(\.?\d+)+', script)
-
-        if ver is not None:
-            ver = ver.group()
-
-        if lib.endswith(".min"):
-            if ver is None:
-                ver = "min"
-
-            lib = lib[:-4]
-
-        output.append([script, lib, ver])
-
-    return output
-
-def buildCpe(lib, ver):
-    cpe = "cpe:2.3:*:*:%s" % lib
-    if ver is not None and len(ver) > 0:
-        cpe += ":%s" % ver
-
-    return cpe
-
-def findCpe(lib):
-    name = lib[1]
-    ver = lib[2]
-    cpe = ""
-    while True:
-        while True:
-            cpe = buildCpe(name, ver)
-            site = requests.get("https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=%s" % urllib.parse.quote(cpe))
-            result = re.search(r'(?<=<strong data-testid="cpe-matching-records-count">)\d+(?=<\/strong>)', site.text).group()
-            if int(result) > 0:
-                return cpe
-
-            if ver is None:
-                ver = lib[2]
-                break
-
-            if ver.find(".") > 0:
-                ver = ".".join((ver.split(".")[:-1]))
-            else:
-                ver = None
-
-        if name is not None and name.find(".") > 0:
-            name = ".".join((name.split(".")[:-1]))
-        elif ver is not None and ver.find("-") > 0:
-            ver = "-".join((name.split("-")[:-1]))
-        else:
-            return None
-
-    return None
-
-def printLib(lib):
-    print('=== Lib ===')
-    print("Match:", lib[0])
-    print("Possible Lib:", lib[1])
-    print("Possible Version:", lib[2])
-
-list = getSitePossibleLibList("https://s.student.pwr.edu.pl/iwc_static/c11n/login_student_pwr_edu_pl.html?lang=pl&3.0.1.3.0_16070546&svcs=abs,mail,calendar,c11n")
+export_to_csv("testFile", tmp)
